4f0043a702776f7358b071116b95bd43826f1d37
RazelRaz <razel16dcm@gmail.com>
2023-06-18 16:00:48 +0600

    DOM textcontent and style

---
+++ dom.js
@@ -0,0 +1,5 @@
+const loremId = document.getElementById('loremId');
+loremId.textContent = 'DOM is handling Me.';
+loremId.style.color = 'red';
+loremId.style.fontWeight = 'bold';
+loremId.style.fontSize = '24px'
