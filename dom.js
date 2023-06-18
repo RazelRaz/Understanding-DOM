cf52e0df845f42f63b43dfa79564bb8b8c079694
RazelRaz <razel16dcm@gmail.com>
2023-06-18 16:47:08 +0600

    querySlector

--- dom.js
+++ dom.js
@@ -2,4 +2,17 @@ const loremId = document.getElementById('loremId');
 loremId.textContent = 'DOM is handling Me.';
 loremId.style.color = 'red';
 loremId.style.fontWeight = 'bold';
-loremId.style.fontSize = '24px'
+loremId.style.fontSize = '24px';
+
+// nav-item
+const navItems = document.getElementsByClassName('nav-item');
+for (const navItem of navItems) {
+    navItem.style.backgroundColor = "red"
+}
+
+const myForm = document.getElementsByTagName('form');
+console.log(myForm);
+
+//it will catch first a of this page
+const firstA = document.querySelector('a');
+firstA.style.color ='green';
